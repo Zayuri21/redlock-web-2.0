4b7e0583a895572a0a9661e5d93f68a07d34eb8a
Zayuri21 <85942968+Zayuri21@users.noreply.github.com>
2023-05-02 02:00:59 +0700

    Add files via upload

---
+++ User_number.php
@@ -0,0 +1,20 @@
+<?php
+$host = 'db'; // hostname yang digunakan server database
+$db_name = 'redlock-web-2.0'; // nama database yang ingin dihubungkan
+$username = 'root'; // username digunakan ketika ingin mengakses database
+$password = 'root'; // password digunakan ketika ingin mengakses database
+
+$conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+// membuat koneksi ke database dengan mengspesifikasi host, nama, username, password
+// PDO = PHP Data Objects
+
+$stmt = $conn->prepare("SELECT COUNT(*) FROM users");
+// $stmt = store, $conn->prepare = query SQL baru
+              // SELECT COUNT(*) FROM users = menghitung banyak barisan pada tabel 'users'
+$stmt->execute(); // menjalankan query
+
+$result = $stmt->fetch(); // hasil dair query, semacam return value
+$total_users = $result[0];
+
+echo "Total number of users: $total_users";
+?>
